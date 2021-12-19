8c1cb15f23fd805ad1d29e4104cd86d453df21f9
bryahchin.mihail <bryahchin.mihail@otr.ru>
2021-12-19 16:35:54 +0300

    DZ14 the end

--- src/test/java/otus/orm/exp/config/MigrateLibraryTest.java
+++ src/test/java/otus/orm/exp/config/MigrateLibraryTest.java
@@ -1,9 +1,44 @@
 package otus.orm.exp.config;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @SpringBatchTest
+@SpringBootTest
 public class MigrateLibraryTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @BeforeEach
+    void clearMetaData() {
+        jobRepositoryTestUtils.removeJobExecutions();
+    }
+
+    @Test
+    public void testMigrate() throws Exception {
+        Job job = jobLauncherTestUtils.getJob();
+        assertThat(job).isNotNull()
+                .extracting(Job::getName)
+                .isEqualTo("migrate");
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(new JobParameters());
+        assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
+    }
 }
